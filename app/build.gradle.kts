694edff4ec9d80a8ca550564d77a27fd3dbe6eed
nikita_ch <nikita_ch@maximail.org>
2023-08-15 17:42:53 +0500

    Connecting the necessary libraries, setting up the color scheme of the project.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,9 @@ android {
             )
         }
     }
+    buildFeatures {
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -36,6 +39,10 @@ android {
 }
 
 dependencies {
+    val roomVersion = "2.5.2"
+    val retrofitVersion = "2.9.0"
+    val gsonConverterVersion = "2.6.4"
+    val googleMapsVersion = "18.1.0"
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -44,4 +51,14 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //Chip Navigation Bar
+    implementation("com.github.ismaeldivita:chip-navigation-bar:1.4.0")
+    //RoomDB
+    implementation ("androidx.room:room-runtime:$roomVersion")
+    annotationProcessor ("androidx.room:room-compiler:$roomVersion")
+    //Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation ("com.squareup.retrofit2:converter-gson:$gsonConverterVersion")
+    //Google Maps
+    implementation ("com.google.android.gms:play-services-maps:$googleMapsVersion")
 }
--- app/src/main/java/com/example/ip_search_app/HostActivity.kt
+++ app/src/main/java/com/example/ip_search_app/HostActivity.kt
@@ -3,7 +3,7 @@ package com.example.ip_search_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
